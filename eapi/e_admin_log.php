d3fd5b608898b990fbf4cc145f35f31696203228
saadbhati1002 <saadbhati1002@gmail.com>
2024-06-11 18:39:27 +0530

    feat: login issue solved

--- eapi/e_admin_log.php
+++ eapi/e_admin_log.php
@@ -2,20 +2,20 @@
 require dirname( dirname(__FILE__) ).'/include/eventconfig.php';
 header('Content-type: text/json');
 $data = json_decode(file_get_contents('php://input'), true);
-if($data['username'] == ''  or $data['password'] == '')
+if($data['email'] == ''  or $data['password'] == '')
 {
     $returnArr = array("ResponseCode"=>"401","Result"=>"false","ResponseMsg"=>"Something Went Wrong!");
 }
 else
 {
-    $username = strip_tags(mysqli_real_escape_string($event,$data['username']));
+    $email = strip_tags(mysqli_real_escape_string($event,$data['email']));
     $password = strip_tags(mysqli_real_escape_string($event,$data['password']));
     
-$chek = $event->query("select * from admin where username='".$username."'  and password='".$password."'");
+$chek = $event->query("select * from admin where email='".$email."'  and password='".$password."'");
 
 if($chek->num_rows != 0)
 {
-    $c = $event->query("select * from admin where username='".$username."'   and password='".$password."'");
+    $c = $event->query("select * from admin where email='".$email."'   and password='".$password."'");
     $c = $c->fetch_assoc();
 
 	
