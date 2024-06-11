54ad73221d38517def5f8291c47d5fc3fbc541df
saadbhati1002 <saadbhati1002@gmail.com>
2024-06-11 18:42:03 +0530

    feat: admin detail check api

---
+++ eapi/e_admin_mobile_check.php
@@ -0,0 +1,29 @@
+<?php 
+require dirname( dirname(__FILE__) ).'/include/eventconfig.php';
+
+$data = json_decode(file_get_contents('php://input'), true);
+if($data['mobile'] == ''or $data['email'] == '')
+{
+    $returnArr = array("ResponseCode"=>"401","Result"=>"false","ResponseMsg"=>"Something Went Wrong!");
+}
+else
+{
+    $mobile = strip_tags(mysqli_real_escape_string($event,$data['mobile']));
+    $email = strip_tags(mysqli_real_escape_string($event,$data['email']));
+    
+    
+$chek = $event->query("select * from admin where mobile='".$mobile."'")->num_rows;
+$chek1 = $event->query("select * from admin where email='".$email."'")->num_rows;
+
+if($chek != 0)
+{
+	$returnArr = array("ResponseCode"=>"401","Result"=>"false","ResponseMsg"=>"Already Exist Mobile Number!");
+}else if($chek1 != 0){
+    	$returnArr = array("ResponseCode"=>"401","Result"=>"false","ResponseMsg"=>"Already Exist Email!");
+}
+else 
+{
+	$returnArr = array("ResponseCode"=>"200","Result"=>"true","ResponseMsg"=>"New Number!");
+}
+}
+echo json_encode($returnArr);
