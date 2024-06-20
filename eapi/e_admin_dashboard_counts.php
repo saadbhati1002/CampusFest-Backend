fa1d08431deba00c9234ab263bb0fdc7e2c8b1b1
saadbhati1002 <saadbhati1002@gmail.com>
2024-06-20 16:18:35 +0530

    feat: dashboard Api response change

--- eapi/e_admin_dashboard_counts.php
+++ eapi/e_admin_dashboard_counts.php
@@ -12,31 +12,36 @@ if ($uid == '') {
     return;
 }
 
-if ($uid == '' or checkAdmin($uid) <= 0) {
+// if ($uid == '' or checkAdmin($uid) <= 0) {
 
-    echo json_encode(array("ResponseCode" => "401", "Result" => "false", "ResponseMsg" => "Unauthorized"));
-    return;
-}
+//     echo json_encode(array("ResponseCode" => "401", "Result" => "false", "ResponseMsg" => "Unauthorized"));
+//     return;
+// }
 
-$admin_type = getAdminType($uid);
+// $admin_type = getAdminType($uid);
 
 $response_data = [
     'ResponseCode' => "200",
     'Result' => "true",
-    'ResponseMsg' => ''
+    'ResponseMsg' => 'Dashboard count get successfully',
+    "Counts"=>[
+         "total_categories"=>$event->query("select * from tbl_cat")->num_rows,
+    "total_event"=>$event->query("select * from tbl_event")->num_rows,
+    "total_pages"=>$event->query("select * from tbl_page")->num_rows,
+    "total_faq_categories"=>$event->query("select * from faq_cat")->num_rows,
+    "total_faqs"=>$event->query("select * from tbl_faq")->num_rows,
+    "total_users"=>$event->query("select * from tbl_user")->num_rows,
+    "total_offers"=>$event->query("select * from tbl_coupon")->num_rows,
+    "total_tickets"=>$event->query("select * from tbl_ticket where ticket_type='Completed'")->num_rows,
+    // "total_sales"=>number_format((float)$event->query("select sum(`total_amt`) as total_sales from tbl_ticket where ticket_type='Completed'")->fetch_assoc()['total_sales'], 2, '.', '') . $set['currency'],
+    "total_admins"=>$event->query("select * from admin")->num_rows,
+    ]
+   
 ];
+// $dashboard_data['total_gallery_images'] = $event->query("select * from tbl_gallery")->num_rows;
+// $dashboard_data['total_tickets'] = $event->query("select * from tbl_ticket where ticket_type='Completed'")->num_rows;
+// $dashboard_data['total_sales'] = number_format((float)$event->query("select sum(`total_amt`) as total_sales from tbl_ticket where ticket_type='Completed'")->fetch_assoc()['total_sales'], 2, '.', '') . $set['currency'];
+// $dashboard_data['total_admins'] = $event->query("select * from admin where admin_type='SUB_ADMIN'")->num_rows;
 
-$dashboard_data['total_categories'] = $event->query("select * from tbl_cat")->num_rows;
-$dashboard_data['total_event'] = $event->query("select * from tbl_event")->num_rows;
-$dashboard_data['total_pages'] = $event->query("select * from tbl_page")->num_rows;
-$dashboard_data['total_faq_categories'] = $event->query("select * from faq_cat")->num_rows;
-$dashboard_data['total_faqs'] = $event->query("select * from tbl_faq")->num_rows;
-$dashboard_data['total_users'] = $event->query("select * from tbl_user")->num_rows;
-$dashboard_data['total_gallery_images'] = $event->query("select * from tbl_gallery")->num_rows;
-$dashboard_data['total_offers'] = $event->query("select * from tbl_coupon")->num_rows;
-$dashboard_data['total_tickets'] = $event->query("select * from tbl_ticket where ticket_type='Completed'")->num_rows;
-$dashboard_data['total_sales'] = number_format((float)$event->query("select sum(`total_amt`) as total_sales from tbl_ticket where ticket_type='Completed'")->fetch_assoc()['total_sales'], 2, '.', '') . $set['currency'];
-$dashboard_data['total_admins'] = $event->query("select * from admin where admin_type='SUB_ADMIN'")->num_rows;
-
-$response_data['dashboard_counts'] = $dashboard_data;
+// $response_data['dashboard_counts'] = $dashboard_data;
 echo json_encode($response_data);
