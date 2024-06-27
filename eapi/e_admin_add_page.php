36201a4e7ef2bf290f09b989a2c05b0f26263a0f
saadbhati1002 <saadbhati1002@gmail.com>
2024-06-27 12:36:28 +0530

    feat: get page api

---
+++ eapi/e_admin_add_page.php
@@ -0,0 +1,66 @@
+<?php
+require dirname(dirname(__FILE__)) . '/include/eventmania.php';
+
+header('Content-type: text/json');
+if ($_SERVER['REQUEST_METHOD'] != 'POST') {
+    echo json_encode(array("ResponseCode" => "405", "Result" => "false", "ResponseMsg" => "Method not allowed"));
+    return;
+}
+
+$uid = isset($_SERVER['HTTP_UID']) ? $_SERVER['HTTP_UID'] : '';
+if ($uid == '') {
+    echo json_encode(array("ResponseCode" => "401", "Result" => "false", "ResponseMsg" => "Unauthorized"));
+    return;
+}
+
+$uid = isset($_GET['uid']) ? $_GET['uid'] : '';
+$data = json_decode(file_get_contents('php://input'), true);
+
+
+// if ($uid == '' or checkAdmin($uid) <= 0) {
+//     echo json_encode(array("ResponseCode" => "401", "Result" => "false", "ResponseMsg" => "Unauthorized"));
+//     return;
+// }
+
+$required_fields = ['title', 'status', 'img', 'cover_img'];
+
+// $validation_errors = [];
+// foreach ($required_fields as $field) {
+//     if (!array_key_exists($field, $data)) {
+//         array_push($validation_errors, "The $field is required.");
+//     }
+// }
+
+// if (count($validation_errors)) {
+//     echo json_encode(array("ResponseCode" => "422", "Result" => "false", "ResponseMsg" => "Invalid request", 'validation_errors' => $validation_errors));
+//     return;
+// }
+$table_name = "tbl_page";
+$fields = [
+    'title','description', 'status'
+];
+
+$category_data = [
+    'title' => $event->real_escape_string($data['title']),
+    'description' => $event->real_escape_string($data['description']),
+    'status' => $event->real_escape_string($data['status']),
+    
+
+];
+
+
+
+try {
+
+    $eventmedia = new Eventmania();
+    $result = $eventmedia->eventinsertdata_Api($fields, $category_data, $table_name);
+    if ($result) {
+        echo json_encode(array("ResponseCode" => "200", "Result" => "true", "ResponseMsg" => "Page has been added successfully."));
+    }
+} catch (Exception $e) {
+    echo json_encode(array("ResponseCode" => "400", "Result" => "false", "ResponseMsg" => $e->getMessage()));
+    return;
+}
+
+
+
