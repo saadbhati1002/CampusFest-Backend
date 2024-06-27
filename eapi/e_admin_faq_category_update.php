b49b2c5a5d06cdf99b80321436d7321bcac59c3f
saadbhati1002 <saadbhati1002@gmail.com>
2024-06-27 09:09:46 +0530

    feat: faq category update

---
+++ eapi/e_admin_faq_category_update.php
@@ -0,0 +1,64 @@
+<?php
+require dirname(dirname(__FILE__)) . '/include/eventmania.php';
+
+header('Content-type: text/json');
+if ($_SERVER['REQUEST_METHOD'] != 'PUT') {
+    echo json_encode(array("ResponseCode" => "405", "Result" => "false", "ResponseMsg" => "Method not allowed"));
+    return;
+}
+
+$uid = isset($_SERVER['HTTP_UID']) ? $_SERVER['HTTP_UID'] : '';
+if ($uid == '') {
+    echo json_encode(array("ResponseCode" => "401", "Result" => "false", "ResponseMsg" => "Unauthorized"));
+    return;
+}
+
+$category_id = isset($_GET['category_id']) ? $_GET['category_id'] : '';
+if ($category_id == '') {
+    echo json_encode(array("ResponseCode" => "404", "Result" => "false", "ResponseMsg" => "Category not found"));
+    return;
+}
+
+$data = json_decode(file_get_contents('php://input'), true);
+
+
+// if ($uid == '' or checkAdmin($uid) <= 0) {
+//     echo json_encode(array("ResponseCode" => "401", "Result" => "false", "ResponseMsg" => "Unauthorized"));
+//     return;
+// }
+
+$required_fields = ['title', 'status'];
+
+// $validation_errors = [];
+// foreach ($required_fields as $field) {
+//     if (!array_key_exists($field, $data)) {
+//         array_push($validation_errors, "The $field is required.");
+//     }
+// }
+
+// if (count($validation_errors)) {
+//     echo json_encode(array("ResponseCode" => "422", "Result" => "false", "ResponseMsg" => "Invalid request", 'validation_errors' => $validation_errors));
+//     return;
+// }
+$table_name = "faq_cat";
+$fields = [
+    'title', 'status'
+];
+
+
+$category_data = [
+    'title' => $data['title'],
+    'status' => $data['status']
+];
+
+
+try {
+
+    $eventmedia = new Eventmania();
+    $result = $eventmedia->eventupdateData_Api($category_data, $table_name, "where id = $category_id");
+    if ($result) {
+        echo json_encode(array("ResponseCode" => "200", "Result" => "true", "ResponseMsg" => "Faq category has been added successfully."));
+    }
+} catch (Exception $e) {
+    echo json_encode(array("ResponseCode" => "400", "Result" => "false", "ResponseMsg" => $e->getMessage()));
+}
